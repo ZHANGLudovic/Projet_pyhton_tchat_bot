a856d147f482c1fdca34053e548dec14bf020935
RayaneBsr <152068899+RayaneBsr@users.noreply.github.com>
2023-11-26 17:49:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from function import *
 
+# Récupération des noms de fichiers et nettoyage des noms
 if __name__ == '__main__':
     directory = "./speeches"
     files_names = list_of_files(directory, "txt")
